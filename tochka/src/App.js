5eec9bf82eefa6bc895c4f558df6c248375d33d5
RadostinKovachev <radostin.kova4ev@abv.bg>
2022-09-01 16:26:57 +0300

    {add} push

--- tochka/src/App.js
+++ tochka/src/App.js
@@ -6,8 +6,18 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { createBox } from '@mui/system';
+import React, {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(4)
+  function descrementsCount() {
+    setCount(prevCount => prevCount - 1)
+  }
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1)
+    
+  }
   return (
     <div className="App">
      <Box sx={{ flexGrow: 1 }}>
@@ -31,8 +41,17 @@ function App() {
     </Box>
       <h1>Важна новина от последния час:</h1>
       <h3>Не знам каква е новината, но благодаря Ви, че си загубихте времето :)</h3>
+      
+        
+      
+      <Button variant="contained">+</Button>
+      <span>0</span>
+      <Button variant="contained">-</Button>
     </div>
+
   );
+  
+  
 }
 
 
