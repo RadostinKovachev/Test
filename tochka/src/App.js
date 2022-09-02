2a03abad9759b21b3aba039baf574f24b054b25d
RadostinKovachev <radostin.kova4ev@abv.bg>
2022-09-02 11:32:15 +0300

    [remove] AppBar

--- tochka/src/App.js
+++ tochka/src/App.js
@@ -1,58 +1,46 @@
 import './App.css';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { createBox } from '@mui/system';
 import React, {useState} from 'react';
 
-function App() {
-  const [count, setCount] = useState(4)
-  function descrementsCount() {
-    setCount(prevCount => prevCount - 1)
-  }
-  function incrementCount() {
-    setCount(prevCount => prevCount + 1)
-    
-  }
+
+function App(props) {
+  const [show, setShow] = useState (false)
+  const [number, setNumber] = useState (0)
   return (
     <div className="App">
-     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Новините днес
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-      <h1>Важна новина от последния час:</h1>
-      <h3>Не знам каква е новината, но благодаря Ви, че си загубихте времето :)</h3>
+     <Button onClick={() =>
+     setShow(!show)
+    }
+    variant='contained'
+    >
+      {show==true ? "Hide" : "Show"}
+      {show == true ?
+    </Button>
+    (<div>
+      <h1>Който стигне до по-голямо число, използвайки копчетата ще получи награда</h1>
+      <h5> (Усмивка по радиото)
+      </h5>
       
         
       
-      <Button variant="contained">+</Button>
-      <span>0</span>
-      <Button variant="contained">-</Button>
+     <Button variant="contained" onClick={() => setNumber (number - 1)}
+     >-</Button>
+      <Button variant="contained" onClick={() => setNumber (number + 1)}>+
+      </Button>
+    <h2>{number} </h2>
+    <Button variant='contained' onClick={() => setNumber(0)
+      }
+      >RESET
+      </Button>
+      <h1>Number: {number}</h1>
     </div>
-
+    )
+    :
+    <div></div>
+         } 
+    </div>
   );
-  
-  
-}
+  }
 
 
 export default App;
